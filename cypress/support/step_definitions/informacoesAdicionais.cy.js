77c458fd3beaacd4f72edfa1fded367ffe9b4c7a
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2026-01-16 18:32:27 -0300

    🐛 fix: Correção de arquivos incorretos no repositório

--- cypress/support/step_definitions/informacoesAdicionais.cy.js
+++
@@ -1,7 +0,0 @@
-import { Given, When, And, Then, Before, After } from 'cypress-cucumber-preprocessor/steps'
-import CompletarCadastro from '../pages/completarCadastro.page'
-
-
-And('preencho os demais dados solicitados', () => {
-    CompletarCadastro.preencherInformacoes()
-})
--- cypress/support/step_definitions/login.cy.js
+++
@@ -1,7 +0,0 @@
-import { Given, When, And, Then, Before, After } from 'cypress-cucumber-preprocessor/steps'
-import PreencherDados from '../pages/preencherDados'
-
-And('insiro um nome e um email válido em registrar', () => {
-    PreencherDados.preencherNomeEmail()
-    PreencherDados.redirecionarContinuar()
-})
