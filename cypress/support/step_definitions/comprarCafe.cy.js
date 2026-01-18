73c151cad7fccc2aa91b8b0718827e8f5e444055
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2026-01-18 15:39:14 -0300

    ♻️ refactor: Removendo repetição desnecessário de código

--- cypress/support/step_definitions/comprarCafe.cy.js
+++ cypress/support/step_definitions/comprarCafe.cy.js
@@ -2,7 +2,7 @@ import { Then , And } from 'cypress-cucumber-preprocessor/steps'
 import FinalizarCompraCarrinho from '../pages/finalizarCompra.page'
 
 Then('preenche seu dados para finalizar a compra', () => {
-    FinalizarCompraCarrinho.finalizarCompraTotal()
+    FinalizarCompraCarrinho.finalizarCompra()
 })
 
 And('visualiza a mensagem de compra finalizada com sucesso', () => {
